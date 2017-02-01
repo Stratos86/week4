74cb9a604a95b023a72b7cac11da3eb654bd1838
Stratos86 <m-wiora@kellogg.northwestern.edu>
2017-01-31 21:37:29 -0600

    Commit 1
    Completed Homework

---
+++ todo/todo.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  $(".list-task").on("click", function(event) {
+    event.preventDefault()
+    var elementThatWasClicked = $(this)
+    console.log(elementThatWasClicked)
+    elementThatWasClicked.parent().remove()
+
+  })
+
+})
